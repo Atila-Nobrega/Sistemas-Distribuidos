76f904ce78923fe774a193cb76f3b84b2c40b1c4
Atila-Nobrega <49105715+Atila-Nobrega@users.noreply.github.com>
2022-09-22 18:33:17 -0300

    Chat_implementado

--- Trabalho1/Task1/calc_client.py
+++ Trabalho1/Task1/calc_client.py
@@ -5,14 +5,17 @@ serverAddressPort   = ("127.0.0.1", 20001)
 # Create a UDP socket at client side
 client = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
+print("Exemplo :4 + 5")
+print("Digite 'fim' para encerrar o cliente e servidor"); 
 # Send to server using created UDP socket
 while True:
-    print("Exemplo :4 + 5")
     # here we get the input from the user
     inp = input("Insira o calculo: ")
     # If user wants to terminate
     # the server connection he can type fim
     if inp == "fim":
+        print("Encerrando o cliente e servidor")
+        client.sendto(inp.encode(), serverAddressPort)
         break
     # Here we send the user input
     # to server socket by send Method
@@ -24,3 +27,4 @@ while True:
     print("Escreva fim para finalizar")
 
 client.close()
+print("Cliente encerrado!")
