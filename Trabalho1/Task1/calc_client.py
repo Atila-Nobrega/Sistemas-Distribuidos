3d02709b09d3665aadf8e502de280d282175356d
Atila-Nobrega <49105715+Atila-Nobrega@users.noreply.github.com>
2022-09-22 14:52:55 -0300

    Add files via upload

---
+++ Trabalho1/Task1/calc_client.py
@@ -0,0 +1,26 @@
+import socket
+
+serverAddressPort   = ("127.0.0.1", 20001)
+
+# Create a UDP socket at client side
+client = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+# Send to server using created UDP socket
+while True:
+    print("Exemplo :4 + 5")
+    # here we get the input from the user
+    inp = input("Insira o calculo: ")
+    # If user wants to terminate
+    # the server connection he can type fim
+    if inp == "fim":
+        break
+    # Here we send the user input
+    # to server socket by send Method
+    client.sendto(inp.encode(), serverAddressPort)
+ 
+    # Here we received output from the server socket
+    answer = client.recv(1024)
+    print("Resposta: "+answer.decode())
+    print("Escreva fim para finalizar")
+
+client.close()
---
+++ Trabalho1/Task1/calc_server.py
@@ -0,0 +1,49 @@
+import socket 
+
+localIP = "127.0.0.1"
+localPort = 20001
+bufferSize = 1024 
+
+msgFromServer = "Sucesso conexao com client"
+bytesToSend = str.encode(msgFromServer)
+
+# Create a datagram socket
+server = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+# Bind to address and ip
+server.bind((localIP, localPort))
+print("UDP server up and listening")
+
+# Listen for incoming datagrams
+while True:
+    data = server.recvfrom(bufferSize)
+    message = data[0]
+    address = data[1]
+    msg = message.decode()
+    if msg == 'Over':
+        print("Connection is Over")
+        break
+ 
+    print("Equação recebida")
+    resultado = 0
+    operacao_list = msg.split()
+    oprnd1 = operacao_list[0]
+    operacao = operacao_list[1]
+    oprnd2 = operacao_list[2]
+ 
+    # here we change str to int converstion
+    num1 = int(oprnd1)
+    num2 = int(oprnd2)
+    # Here we are perform  basic arithmetic operacao
+    if operacao == "+":
+        resultado = num1 + num2
+    elif operacao == "-": 
+        resultado = num1 - num2
+    elif operacao == "/":
+        resultado = num1 / num2
+    elif operacao == "*":
+        resultado = num1 * num2
+ 
+    print("Resultado enviado")
+   
+    output = str(resultado)
+    server.sendto(output.encode(), address)
